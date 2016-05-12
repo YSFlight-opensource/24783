8f128f72c1ee2cd31bc8a9ed1ed123db77dbccda
HLH15 <HLH15@users.noreply.github.com>
2016-05-12 16:27:16 -0400

    Delete checker.h

--- sampler2d/checker.h
+++
@@ -1,9 +0,0 @@
-#ifndef CHECKER_IS_INCLUDED
-#define CHECKER_IS_INCLUDED
-
-// Note: OpenGL ES only takes 2^n times 2^n texture.
-const int checker_pattern_wid=8;
-const int checker_pattern_hei=8;
-extern const unsigned char checker_pattern[checker_pattern_wid*checker_pattern_hei*4];
-
-#endif
